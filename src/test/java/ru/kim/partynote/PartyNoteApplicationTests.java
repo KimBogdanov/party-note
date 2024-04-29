389b71bfb997671a58e2a4b2511bfeaab98ff18f
Kim Bogdanov <105812215+KimBogdanov@users.noreply.github.com>
2024-04-29 12:45:35 +0300

    start project

---
+++ src/test/java/ru/kim/partynote/PartyNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kim.partynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
